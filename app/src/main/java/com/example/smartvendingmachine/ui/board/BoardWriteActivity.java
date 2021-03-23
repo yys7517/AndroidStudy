e1d45661e0b17a8ea38fcabd06d742aa2ba7ec26
yys7517 <yys7517@naver.com>
2021-03-24 01:36:55 +0900

    asdsad

---
+++ app/src/main/java/com/example/smartvendingmachine/ui/board/BoardWriteActivity.java
@@ -0,0 +1,17 @@
+package com.example.smartvendingmachine.ui.board;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartvendingmachine.R;
+
+public class BoardWriteActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_write);
+    }
+}
