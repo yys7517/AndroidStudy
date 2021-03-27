1fa51a749db4860e705f33832405ebb4b43761b5
yys7517 <yys7517@naver.com>
2021-03-28 01:31:46 +0900

    fragment 뒤로가기

--- app/src/main/java/com/example/smartvendingmachine/ui/MainActivity.java
+++ app/src/main/java/com/example/smartvendingmachine/ui/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.smartvendingmachine.ui;
 
+import android.app.FragmentTransaction;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import com.example.smartvendingmachine.R;
@@ -21,20 +22,26 @@ public class MainActivity extends AppCompatActivity {
     HomeFragment homeFragment;
     ProfileFragment profileFragment;
 
+    private FragmentTransaction transaction;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.nav_view);
+
 
         //프래그먼트 생성
         boardFragment = new BoardFragment();
         homeFragment = new HomeFragment();
         profileFragment = new ProfileFragment();
 
+        bottomNavigationView = findViewById(R.id.nav_view);
+
         // 처음에 띄울화면 이걸로 기릿~
-        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, homeFragment).commitAllowingStateLoss();
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, homeFragment,"home").commitAllowingStateLoss();
 
         // 바텀 내비게이션 뷰 초기 선택 값 (홈)
         bottomNavigationView.setSelectedItemId(R.id.frag_navigation_home);
@@ -45,21 +52,51 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.frag_navigation_home: {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, homeFragment).commitAllowingStateLoss();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, homeFragment,"home").addToBackStack(null).commitAllowingStateLoss();
                         return true;
                     }
                     case R.id.frag_navigation_board: {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, boardFragment).commitAllowingStateLoss();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, boardFragment,"board").addToBackStack(null).commitAllowingStateLoss();
                         return true;
                     }
                     case R.id.frag_navigation_profile: {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, profileFragment).commitAllowingStateLoss();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, profileFragment,"profile").addToBackStack(null).commitAllowingStateLoss();
                         return true;
                     }
 
-                    default:return false;
+                    default:
+                        return false;
                 }
             }
         });
+
+
+
     }
+
+    public void updateBottomMenu (BottomNavigationView navigation)
+    {
+        if(getSupportFragmentManager().findFragmentByTag("home") != null && getSupportFragmentManager().findFragmentByTag("home").isVisible() ) {
+            bottomNavigationView.getMenu().findItem(R.id.frag_navigation_home).setChecked(true);
+            //bottomNavigationView.setSelectedItemId(R.id.frag_navigation_home);
+        }
+        else if(getSupportFragmentManager().findFragmentByTag("board") != null && getSupportFragmentManager().findFragmentByTag("board").isVisible() ) {
+            bottomNavigationView.getMenu().findItem(R.id.frag_navigation_board).setChecked(true);
+            //bottomNavigationView.setSelectedItemId(R.id.frag_navigation_board);
+        }
+        else if(getSupportFragmentManager().findFragmentByTag("profile") != null && getSupportFragmentManager().findFragmentByTag("profile").isVisible() ) {
+            bottomNavigationView.getMenu().findItem(R.id.frag_navigation_profile).setChecked(true);
+           //bottomNavigationView.setSelectedItemId(R.id.frag_navigation_profile);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        BottomNavigationView bnv = findViewById(R.id.nav_view);
+        updateBottomMenu(bnv);
+
+    }
+
 }
