c2526f4678bb631bf1dbfaa7b425fe94d705f4ac
yys7517 <yys7517@naver.com>
2022-03-08 18:25:03 +0900

    ImageView를 식별할 수 있는 id 부여 및 ImageView 변수 생성.

--- app/src/main/java/com/youngsun/imageview_intent_activity/MainActivity.kt
+++ app/src/main/java/com/youngsun/imageview_intent_activity/MainActivity.kt
@@ -2,10 +2,28 @@ package com.youngsun.imageview_intent_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // FindViewById()를 사용하여 ImageView 객체 생성.
+        val img_1 = findViewById<ImageView>(R.id.img_1)
+        val img_2 = findViewById<ImageView>(R.id.img_2)
+        val img_3 = findViewById<ImageView>(R.id.img_3)
+        val img_4 = findViewById<ImageView>(R.id.img_4)
+        val img_5 = findViewById<ImageView>(R.id.img_5)
+        val img_6 = findViewById<ImageView>(R.id.img_6)
+        val img_7 = findViewById<ImageView>(R.id.img_7)
+
+
+
+
     }
+
+
+
 }
