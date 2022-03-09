c5e684b557679676d75cf792179fc9f81c47d89e
yys7517 <yys7517@naver.com>
2022-03-09 19:01:52 +0900

    각 ImageView에 id를 부여, 이미지를 크게 보여줄 ImageActivity 및 레이아웃 생성.

---
+++ app/src/main/java/com/youngsun/cardview_intent_activity/ImageActivity.kt
@@ -0,0 +1,11 @@
+package com.youngsun.cardview_intent_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
