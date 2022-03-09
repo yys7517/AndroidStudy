79efc4f72797db818db80709e2cb4473cf7a6542
yys7517 <yys7517@naver.com>
2022-03-09 16:40:49 +0900

    Android Handler 사용하여 Splash 화면 3초 출력 후, Intent를 사용하여 MainActivity로 전환

--- app/src/main/java/com/youngsun/cardview_intent_activity/SplashActivity.kt
+++ app/src/main/java/com/youngsun/cardview_intent_activity/SplashActivity.kt
@@ -1,11 +1,18 @@
 package com.youngsun.cardview_intent_activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed( {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
