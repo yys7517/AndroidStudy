efdefcc9296134c8e200986c51d9e562efaea91a
yys7517 <yys7517@naver.com>
2022-03-14 19:00:49 +0900

    RecyclerView의 클릭 리스너 구현.

--- app/src/main/java/com/youngsun/recyclerview_ex/RecyclerViewAdapter.kt
+++ app/src/main/java/com/youngsun/recyclerview_ex/RecyclerViewAdapter.kt
@@ -6,6 +6,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+// ListView와 RecyclerView의 가장 큰 차이점 ?
+// ListView의 경우, 매 번 뷰 바인딩을 하여 성능이 저하된다.
+// RecyclerView의 경우, ViewHolder 패턴을 강제하여, 뷰 바인딩을 한 번만 해주고,
+// 그 후에는 바인딩 된 뷰 객체를 재활용 한다는 장점.
 
 class RecyclerViewAdapter( val items : MutableList<String> ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
@@ -16,7 +20,17 @@ class RecyclerViewAdapter( val items : MutableList<String> ) : RecyclerView.Adap
         return ViewHolder(view)
     }
 
+    interface ItemClick {
+        fun onClick( view : View, position: Int )
+    }
+    var itemClick : ItemClick? = null
+
     override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        if( itemClick != null ) {
+            holder.itemView.setOnClickListener {
+                v -> itemClick?.onClick(v, position)
+            }
+        }
         holder.bindItems( items[position] )
     }
 
@@ -31,4 +45,6 @@ class RecyclerViewAdapter( val items : MutableList<String> ) : RecyclerView.Adap
             rv_text.text = item
         }
     }
+
+
 }
