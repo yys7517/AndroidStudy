6e8d5bfce35ab7571206c8fc593f563cb2e3431d
yys7517 <yys7517@naver.com>
2022-03-18 01:36:33 +0900

    MainActivity에서 Firebase서버와 비동기 방식을 통해 연결하여 ListView를 갱신.

--- app/src/main/java/com/youngsun/diet_memo/MainActivity.kt
+++ app/src/main/java/com/youngsun/diet_memo/MainActivity.kt
@@ -7,14 +7,59 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val list_item = mutableListOf<DataModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.mainLV)
+
+        val adapter = ListViewAdapter( list_item )
+        listView.adapter = adapter
+
+        val database = Firebase.database
+        val myRef = database.getReference("myMemo")
+
+        myRef.child(Firebase.auth.currentUser!!.uid).addValueEventListener( object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // onDataChange() 메서드는 리스너가 연결될 때 한 번 트리거된 후 하위 항목을 포함하여 데이터가 변경될 때마다 다시 트리거됩니다.
+
+                // 초기에 리스트 값을 비워놓고
+                list_item.clear()
+
+                // snapshot에 모든 데이터를 가져와 담는다.
+                // snapshot의 내용을 반복문을 통해 꺼내온다.
+                for( dataModel in snapshot.children ) {
+                    Log.d("Data", dataModel.toString() )
+
+                    list_item.add( dataModel.getValue(DataModel::class.java)!! )     // DataModel 형식으로 값을 변환하여 가져온다.
+                }
+
+                adapter.notifyDataSetChanged()  // 값이 들어가서 데이터가 바뀌었을 수도 있으니 어댑터를 새롭게 만들어줘라.
+               /*
+               val adapter = ListViewAdapter( list_item )
+               listView.adapter = adapter
+               위와 같이 값을 받고, 어댑터를 지정해주려고 하였다.
+
+               위에서 리스트 뷰의 어댑터를 지정해주고 adapter.notifyDataSetChanged()를 사용하여 값을 새로고침 해줘도 된다.
+                 */
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
 
         val writeBtn = findViewById<ImageView>(R.id.writeBtn)
 
@@ -57,7 +102,11 @@ class MainActivity : AppCompatActivity() {
                 // Firebase Realtime database 에 메모 내용과 날짜를 저장.
 
                 val database = Firebase.database
-                val myRef = database.getReference("myMemo")
+                // val myRef = database.getReference("myMemo")     // 값이 저장될 Path.
+
+                // 사용자 별로 uid에 따라 Path를 지정해보자.
+                val myRef = database.getReference("myMemo").child(Firebase.auth.currentUser!!.uid)     // 값이 저장될 Path.
+                // myMemo / uid / DataModel( 날짜, 메모 내용 )
 
                 // 입력된 메모 내용을 가져온다.
                 val myMemo = mAlertDialog.findViewById<EditText>(R.id.edtMemo)?.text.toString()
