12470ee3289eafde2c6eac12441f841dcaddac08
yys7517 <yys7517@naver.com>
2022-03-18 18:16:32 +0900

    RecyclerView 디자인 및 구성 변경, 폰트 적용

--- app/src/main/java/com/youngsun/mango_contents/MainActivity.kt
+++ app/src/main/java/com/youngsun/mango_contents/MainActivity.kt
@@ -24,6 +24,38 @@ class MainActivity : AppCompatActivity() {
 
         items.add(
             ContentsModel(
+                "https://www.mangoplate.com/restaurants/XRoMziImmYCC",
+                "https://mp-seoul-image-production-s3.mangoplate.com/331247/60039_1596540913676_34054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "뉴욕택시디저트"
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/bKBEWmF8MVGb",
+                "https://mp-seoul-image-production-s3.mangoplate.com/705256_1562413869818147.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "동경산책"
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/pVydRWGId3d8",
+                "https://mp-seoul-image-production-s3.mangoplate.com/sources/web/restaurants/398605/1062290_1606269461021?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "피자보이시나"
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/mmhYgR5FVnYH",
+                "https://mp-seoul-image-production-s3.mangoplate.com/516849_1579437536418491.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "경원치킨"
+            )
+        )
+
+        items.add(
+            ContentsModel(
                 "https://www.mangoplate.com/restaurants/ByuIW33rXc",
                 "https://mp-seoul-image-production-s3.mangoplate.com/219116/488522_1639565714575_24422?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
                 "알라프리마"
@@ -32,9 +64,33 @@ class MainActivity : AppCompatActivity() {
 
         items.add(
             ContentsModel(
-                "https://www.mangoplate.com/restaurants/ByuIW33rXc",
-                "https://mp-seoul-image-production-s3.mangoplate.com/219116/488522_1639565714575_24420?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "알라프리마"
+                "https://www.mangoplate.com/restaurants/XRoMziImmYCC",
+                "https://mp-seoul-image-production-s3.mangoplate.com/331247/60039_1596540913676_34054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "뉴욕택시디저트"
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/bKBEWmF8MVGb",
+                "https://mp-seoul-image-production-s3.mangoplate.com/705256_1562413869818147.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "동경산책"
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/pVydRWGId3d8",
+                "https://mp-seoul-image-production-s3.mangoplate.com/sources/web/restaurants/398605/1062290_1606269461021?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "피자보이시나"
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.mangoplate.com/restaurants/mmhYgR5FVnYH",
+                "https://mp-seoul-image-production-s3.mangoplate.com/516849_1579437536418491.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "경원치킨"
             )
         )
 
